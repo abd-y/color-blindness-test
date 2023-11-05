1f814e38886b11148149e1ba17a9ae1780a5860b
abd-y <39744252+abd-y@users.noreply.github.com>
2023-11-05 16:16:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if "d_score" not in st.session_state:
 if "result" not in st.session_state:
     st.session_state["result"] = ''
 
-#here are all the image file names
+#getting all iamge names into a list 
 images = ["images/" + str(i) + ".jpg" for i in range(1, 19)]
 
 def check_answer(answer):
@@ -24,6 +24,11 @@ def check_answer(answer):
     '''
     prolog = Prolog()
     prolog.consult("logic.pl")
+    '''
+    adding the answer to prolog to check if it was true or not,
+    the query function return an object so to know if the answer
+    is true or not we have to convert it to list then boolean
+    '''
     correct_answer = bool(list(prolog.query(f"answer(img{st.session_state.list_index + 1}, {answer})")))
     if correct_answer:
         st.session_state["list_index"] += 1
@@ -47,8 +52,14 @@ def check_color_blind_type(answer):
     prolog will diagonose what type of color blind from the
     image index and the answer
     '''
+    #connecting to prolog
     prolog = Prolog()
     prolog.consult("logic.pl")
+    '''
+    adding the answer to prolog to check if it was true or not
+    the query function return an object so to know if the answer
+    is true or not we have to convert it to list then boolean
+    '''
     correct_answer = bool(list(prolog.query(f"answer(img{st.session_state.list_index + 1}, {answer})")))
     if correct_answer:
         check_answer(answer)
@@ -77,15 +88,18 @@ def check_color_blind_type(answer):
                 st.session_state.result = "You have Deuteranopia."
 
 def display():
+    #display page content
     #display a text in the page
         st.title("Color blindness test")
         #adding a custom style for buttons
+        #inside the markdown function there is a html code
         st.markdown(
             """
             <style>
             button {
                 width: 100px !important;
             },
+            </style>
             """,
             unsafe_allow_html=True,
         )
@@ -93,7 +107,15 @@ def display():
         #the bellow statment is to make the image in the middle of the page
         _, cl, _, _ = st.columns(4)
         if st.session_state["list_index"] < len(images):
-            cl.image(images[st.session_state["list_index"]], width=250)
+            #stop going to the next image when the score is more than or equal 12
+            if st.session_state["list_index"] == 12 and st.session_state["score"] >= 12:
+                cl.image(images[11], width=250)
+            elif st.session_state["list_index"] == 13 and st.session_state["score"] >= 12:
+                cl.image(images[12], width=250)
+            elif st.session_state["list_index"] == 14 and st.session_state["score"] >= 12:
+                cl.image(images[13], width=250)
+            else:
+                cl.image(images[st.session_state["list_index"]], width=250)
         else:
             cl.image(images[len(images) - 1], width=250)
         print(st.session_state)
@@ -111,95 +133,96 @@ def reset():
     st.session_state["d_score"] = 0
     st.session_state["result"] = ''
 
+def end(message):
+    st.session_state.result = message
+    st.write(st.session_state.result)
+    st.button("try again", on_click=reset)
+
 def main():
-    if st.session_state.list_index < 14:
-        col1, col2, col3 = display()
-        #change the button values when the list_index is changed
-        #also each button will call a specific function when it's clicked
-        if st.session_state.list_index == 0:
-            col1.button("10", on_click=check_answer, args=(10,))
-            col2.button("13", on_click=check_answer, args=(13,))
-            col3.button("12", on_click=check_answer, args=(12,))
-        elif st.session_state.list_index == 1:
-            col1.button("8", on_click=check_answer, args=(8,))
-            col2.button("5", on_click=check_answer, args=(5,))
-            col3.button("3", on_click=check_answer, args=(3,))
-        elif st.session_state.list_index == 2:
-            col1.button("9", on_click=check_answer, args=(9,))
-            col2.button("6", on_click=check_answer, args=(6,))
-            col3.button("5", on_click=check_answer, args=(5,))
-        elif st.session_state.list_index == 3:
-            col1.button("70", on_click=check_answer, args=(70,))
-            col2.button("26", on_click=check_answer, args=(26,))
-            col3.button("29", on_click=check_answer, args=(29,))
-        elif st.session_state.list_index == 4:
-            col1.button("2", on_click=check_answer, args=(2,))
-            col2.button("5", on_click=check_answer, args=(5,))
-            col3.button("7", on_click=check_answer, args=(7,))
-        elif st.session_state.list_index == 5:
-            col1.button("9", on_click=check_answer, args=(9,))
-            col2.button("3", on_click=check_answer, args=(3,))
-            col3.button("5", on_click=check_answer, args=(5,))
-        elif st.session_state.list_index == 6:
-            col1.button("15", on_click=check_answer, args=(15,))
-            col2.button("11", on_click=check_answer, args=(11,))
-            col3.button("17", on_click=check_answer, args=(17,))
-        elif st.session_state.list_index == 7:
-            col1.button("9", on_click=check_answer, args=(9,))
-            col2.button("6", on_click=check_answer, args=(6,))
-            col3.button("2", on_click=check_answer, args=(2,))
-        elif st.session_state.list_index == 8:
-            col1.button("3", on_click=check_answer, args=(3,))
-            col2.button("2", on_click=check_answer, args=(2,))
-            col3.button("6", on_click=check_answer, args=(6,))
-        elif st.session_state.list_index == 9:
-            col1.button("75", on_click=check_answer, args=(75,))
-            col2.button("55", on_click=check_answer, args=(55,))
-            col3.button("97", on_click=check_answer, args=(97,))
-        elif st.session_state.list_index == 10:
-            col1.button("9", on_click=check_answer, args=(9,))
-            col2.button("6", on_click=check_answer, args=(6,))
-            col3.button("5", on_click=check_answer, args=(5,))
-        elif st.session_state.list_index == 11:
-            col1.button("7", on_click=check_answer, args=(7,))
-            col2.button("2", on_click=check_answer, args=(2,))
-            col3.button("9", on_click=check_answer, args=(9,))
-        elif st.session_state.list_index == 12:
-            col1.button("5", on_click=check_answer, args=(5,))
-            col2.button("2", on_click=check_answer, args=(2,))
-            col3.button("nothing", on_click=check_answer, args=("nothing",))
-        elif st.session_state.list_index == 13:
-            col1.button("7", on_click=check_answer, args=(7,))
-            col2.button("2", on_click=check_answer, args=(2,))
-            col3.button("nothing", on_click=check_answer, args=("nothing",))
-    else:
-        if st.session_state.list_index == 14 and st.session_state.score > 12:
-            st.session_state.list_index = 13
-            display()
-            st.session_state.result = "You're not color blind"
-            st.write(st.session_state.result)
-            st.button("try again", on_click=reset)
-        else:
-            #continue the test to diagonose what type of color blind
-            col1, col2, col3 = display()
-            if st.session_state.list_index == 14:
-                col1.button("26", on_click=check_color_blind_type, args=(26,))
-                col2.button("6", on_click=check_color_blind_type, args=(6,))
-                col3.button("2", on_click=check_color_blind_type, args=(2,))
-            elif st.session_state.list_index == 15:
-                col1.button("2", on_click=check_color_blind_type, args=(2,))
-                col2.button("42", on_click=check_color_blind_type, args=(42,))
-                col3.button("4", on_click=check_color_blind_type, args=(4,))
-            elif st.session_state.list_index == 16:
-                col1.button("5", on_click=check_color_blind_type, args=(5,))
-                col2.button("3", on_click=check_color_blind_type, args=(3,))
-                col3.button("35", on_click=check_color_blind_type, args=(35,))
-            elif st.session_state.list_index == 17:
-                col1.button("96", on_click=check_color_blind_type, args=(96,))
-                col2.button("6", on_click=check_color_blind_type, args=(6,))
-                col3.button("9", on_click=check_color_blind_type, args=(9,))
-            elif st.session_state.list_index > 17:
-                st.write(st.session_state.result)
-                st.button("try again", on_click=reset)
+    col1, col2, col3 = display()
+    #change the button values when the list_index is changed
+    #also each button will call a specific function when it's clicked
+    if st.session_state.list_index == 0:
+        col1.button("10", on_click=check_answer, args=(10,))
+        col2.button("13", on_click=check_answer, args=(13,))
+        col3.button("12", on_click=check_answer, args=(12,))
+    elif st.session_state.list_index == 1:
+        col1.button("8", on_click=check_answer, args=(8,))
+        col2.button("5", on_click=check_answer, args=(5,))
+        col3.button("3", on_click=check_answer, args=(3,))
+    elif st.session_state.list_index == 2:
+        col1.button("9", on_click=check_answer, args=(9,))
+        col2.button("6", on_click=check_answer, args=(6,))
+        col3.button("5", on_click=check_answer, args=(5,))
+    elif st.session_state.list_index == 3:
+        col1.button("70", on_click=check_answer, args=(70,))
+        col2.button("26", on_click=check_answer, args=(26,))
+        col3.button("29", on_click=check_answer, args=(29,))
+    elif st.session_state.list_index == 4:
+        col1.button("2", on_click=check_answer, args=(2,))
+        col2.button("5", on_click=check_answer, args=(5,))
+        col3.button("7", on_click=check_answer, args=(7,))
+    elif st.session_state.list_index == 5:
+        col1.button("9", on_click=check_answer, args=(9,))
+        col2.button("3", on_click=check_answer, args=(3,))
+        col3.button("5", on_click=check_answer, args=(5,))
+    elif st.session_state.list_index == 6:
+        col1.button("15", on_click=check_answer, args=(15,))
+        col2.button("11", on_click=check_answer, args=(11,))
+        col3.button("17", on_click=check_answer, args=(17,))
+    elif st.session_state.list_index == 7:
+        col1.button("9", on_click=check_answer, args=(9,))
+        col2.button("6", on_click=check_answer, args=(6,))
+        col3.button("2", on_click=check_answer, args=(2,))
+    elif st.session_state.list_index == 8:
+        col1.button("3", on_click=check_answer, args=(3,))
+        col2.button("2", on_click=check_answer, args=(2,))
+        col3.button("6", on_click=check_answer, args=(6,))
+    elif st.session_state.list_index == 9:
+        col1.button("75", on_click=check_answer, args=(75,))
+        col2.button("55", on_click=check_answer, args=(55,))
+        col3.button("97", on_click=check_answer, args=(97,))
+    elif st.session_state.list_index == 10:
+        col1.button("9", on_click=check_answer, args=(9,))
+        col2.button("6", on_click=check_answer, args=(6,))
+        col3.button("5", on_click=check_answer, args=(5,))
+    elif st.session_state.list_index == 11:
+        col1.button("7", on_click=check_answer, args=(7,))
+        col2.button("2", on_click=check_answer, args=(2,))
+        col3.button("9", on_click=check_answer, args=(9,))
+    elif st.session_state.list_index == 12 and st.session_state.score < 12:
+        col1.button("5", on_click=check_answer, args=(5,))
+        col2.button("2", on_click=check_answer, args=(2,))
+        col3.button("nothing", on_click=check_answer, args=("nothing",))
+    elif st.session_state.list_index == 12 and st.session_state.score >= 12:
+        end("You're not color blind.")
+    elif st.session_state.list_index == 13 and st.session_state.score < 12:
+        col1.button("7", on_click=check_answer, args=(7,))
+        col2.button("2", on_click=check_answer, args=(2,))
+        col3.button("nothing", on_click=check_answer, args=("nothing",))
+    elif st.session_state.list_index == 13 and st.session_state.score >= 12:
+        end("You're not color blind.")
+    elif st.session_state.list_index == 14 and st.session_state.score >= 12:
+        end("You're not color blind.")
+    #continue the test to diagonose what type of color blind if the score < 12
+    elif st.session_state.list_index == 14 and st.session_state.score < 12:
+        col1.button("26", on_click=check_color_blind_type, args=(26,))
+        col2.button("6", on_click=check_color_blind_type, args=(6,))
+        col3.button("2", on_click=check_color_blind_type, args=(2,))
+    elif st.session_state.list_index == 15:
+        col1.button("2", on_click=check_color_blind_type, args=(2,))
+        col2.button("42", on_click=check_color_blind_type, args=(42,))
+        col3.button("4", on_click=check_color_blind_type, args=(4,))
+    elif st.session_state.list_index == 16:
+        col1.button("5", on_click=check_color_blind_type, args=(5,))
+        col2.button("3", on_click=check_color_blind_type, args=(3,))
+        col3.button("35", on_click=check_color_blind_type, args=(35,))
+    elif st.session_state.list_index == 17:
+        col1.button("96", on_click=check_color_blind_type, args=(96,))
+        col2.button("6", on_click=check_color_blind_type, args=(6,))
+        col3.button("9", on_click=check_color_blind_type, args=(9,))
+    elif st.session_state.list_index > 17:
+        st.write(st.session_state.result)
+        st.button("try again", on_click=reset)
 
 main()
